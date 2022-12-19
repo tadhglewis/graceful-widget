b88fbd25b9db43d31d5266f2a162f02852364a8e
Tadhg Lewis <tadhg.lewis@outlook.com>
2022-12-19 12:37:52 +1100

    Init

---
+++ src/App/App.tsx
@@ -0,0 +1,19 @@
+import 'braid-design-system/reset';
+
+import { BraidProvider } from 'braid-design-system';
+import apac from 'braid-design-system/themes/apac';
+import React, { StrictMode } from 'react';
+
+import { NextSteps } from './NextSteps';
+
+interface AppProps {
+  environment: 'development' | 'production';
+}
+
+export default ({ environment }: AppProps) => (
+  <StrictMode>
+    <BraidProvider theme={apac}>
+      <NextSteps environment={environment} />
+    </BraidProvider>
+  </StrictMode>
+);
---
+++ src/client.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { hydrateRoot } from 'react-dom/client';
+
+import App from './App/App';
+import { ClientContext } from './types';
+
+export default ({ environment }: ClientContext) => {
+  hydrateRoot(
+    document.getElementById('app')!,
+    <App environment={environment} />,
+  );
+};
